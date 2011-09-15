da98bca7e4ebafd1190217817194106a2ccee7af
Fotios Lindiakos <fotios@redhat.com>
2011-09-15 13:05:48 -0400

    Added bash script to automate the build process

---
+++ makerpm.sh
@@ -0,0 +1,34 @@
+function makerpm() {
+  GEMNAME=$@;
+  RPMDIR="$HOME/rpmbuild"
+  SPECNAME="$RPMDIR/SPECS/rubygem-$GEMNAME.spec";
+
+  # Create the required directories
+  mkdir -p $RPMDIR/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+  # Save the current directory and move
+  pushd "$RPMDIR/SOURCES"
+
+  # Get the gemfile
+  gem fetch $GEMNAME;
+
+  # Generate base specfile
+  gem2rpm "$GEMNAME"* > $SPECNAME;
+
+  # Edit the spec.
+  # Inside vim run this before quitting to make sure build succeeds: 
+  #     rpmbuild -ba %   
+  vim $SPECNAME;
+
+  # Run a final build just to make sure it works
+  rpmbuild -ba $SPECNAME
+
+  # Add and commit back to the git repo
+  git add "$RPMDIR"/**/*"$GEMNAME"* "$RPMDIR"/**/*/*"$GEMNAME"*
+  git commit -am "Added $GEMNAME";
+
+  # Go back to the directory
+  popd
+}
+
+makerpm $@
