9c0528baa97910e0ad7cdfc2d85712cfdc1f029a
Fotios Lindiakos <fotios@redhat.com>
2012-02-09 12:23:54 -0500

    Made script cleanup build tree Do not automatically add to git

--- makerpm.sh
+++ makerpm.sh
@@ -29,11 +29,11 @@ function makerpm() {
   vim $SPECNAME;
 
   # Run a final build just to make sure it works
-  rpmbuild -ba $SPECNAME
+  rpmbuild -ba --clean $SPECNAME
 
   # Add and commit back to the git repo
-  git add "$RPMDIR"/**/*"$GEMNAME"* "$RPMDIR"/**/*/*"$GEMNAME"*
-  git commit -am "Added $GEMNAME";
+  #git add "$RPMDIR"/**/*"$GEMNAME"* "$RPMDIR"/**/*/*"$GEMNAME"*
+  #git commit -am "Added $GEMNAME";
 
   # Go back to the directory
   popd
