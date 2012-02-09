9e7baf1349dbbccdd22befa895748cf41f5d7c53
Fotios Lindiakos <fotios@redhat.com>
2012-02-09 12:47:58 -0500

    Cleaned up BUILD folder

--- BUILD/rubygem-barista-1.2.1/usr/lib/ruby/gems/1.8/gems/barista-1.2.1/lib/barista/tasks/barista.rake
+++
@@ -1,7 +0,0 @@
-require 'barista/rake_task'
-
-Barista::RakeTask.new do |t|
-  t.namespace = :barista
-  t.task_name = :brew
-  t.rails     = true
-end
--- BUILD/rubygem-barista-1.2.1/usr/lib/ruby/gems/1.8/gems/barista-1.2.1/lib/barista/version.rb
+++
@@ -1,8 +0,0 @@
-module Barista
-  module Version
-    MAJOR  = 1
-    MINOR  = 2
-    PATCH  = 1
-    STRING = [MAJOR, MINOR, PATCH].join(".")
-  end
-end
